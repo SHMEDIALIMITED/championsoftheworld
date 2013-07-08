48c1275a5aa7e5238b78218c172d2e5106695e60
Patrick Wolleb <patrick.wolleb@shmedia.co.uk>
2013-07-08 17:37:10 +0100

    CD test

--- src/app/controllers/pages.js
+++ src/app/controllers/pages.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
 	api.index = function(req, res) {
 		res.render('index', {layout:false,locals:{
 			version:config.version, 
-			title: 'SH MEDIA', description:'Web App Template', 
+			title: 'Champions of the World', description:'Flag WebGL', 
 			fbAppID: config.facebook.clientID}});
 	}
 
