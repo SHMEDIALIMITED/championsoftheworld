e8dc7fe6632c65616335db426070241f04ce0eb6
Patrick Wolleb <patrick.wolleb@shmedia.co.uk>
2013-07-10 11:11:26 +0100

    Removed hardcoded Twitter API keys/secrets. Passing them via node process vars Created production twitter app

--- src/config/config.js
+++ src/config/config.js
@@ -26,10 +26,10 @@ module.exports = {
       db: process.env.MONGOLAB_URI,
       host : 'http://champions-of-the-world.herokuapp.com',
       twitter: {
-        consumer_key: 'u6GPk5YU3FlI5ulxesBPg',
-        consumer_secret: '0kT0ITCJ2p37hXh4166KTM4eZrKp9vifQKOh3Ls0',
-        access_token_key: '22387661-xyP2A4NB7WbSJ5inouaLvXFncnLI0Tk7BwoquIpRw',
-        access_token_secret: 'OzQSN1gWAiOOb4IazfSttpX4Qv0uoVDV4Fwox7GjqI'
+        consumer_key: process.env.TWITTER_KEY,
+        consumer_secret: process.env.TWITTER_SECRET,
+        access_token_key: process.env.TWITTER_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_TOKEN_SECRET
       },
       hashtag : 'championsoftheworld'
 
